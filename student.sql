a932f26cb5f181bc9068a358db45a0cea962b5db
ArindamBhatt <57433071+arindam9864@users.noreply.github.com>
2026-02-14 14:16:27 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,7 @@
+INSERT INTO STUDENT (ROLL_NO, NAME, ADDRESS, PHONE, AGE) VALUES
+('1', 'RAM', 'DELHI', '******', 18),
+('2', 'RAMESH', 'GURGAON', '******', 18),
+('3', 'SUJIT', 'ROHTAK', '******', 20),
+('4', 'SURESH', 'DELHI', '******', 18),
+('5', 'AMAN', 'ROHTAK', '******', 20),
+('6', 'HARSH', 'GURGAON', '******', 18);
