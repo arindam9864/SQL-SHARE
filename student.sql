281b4694fd201655f4480353a72bb0c1a8dad6b6
ArindamBhatt <57433071+arindam9864@users.noreply.github.com>
2026-02-27 21:19:26 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -1,15 +1,9 @@
-CREATE TABLE IF NOT EXISTS Orders(
-  ord_no TEXT PRIMARY KEY,
-  purch_amt TEXT,
-  ord_date TEXT,
-  customer_id TEXT,
-  Salesman_id TEXT
-);
-INSERT INTO Orders(ord_no,purch_amt,ord_date,customer_id,Salesman_id)
-VALUES
-  ("70001","150.5","2012-10-05","3005","5002"),
-  ("70009","270.65","2012-09-10","3001","5001"),
-  ("70002","65.26","2012-10-05","3002","5003"),
-  ("70004","110.5","2012-08-17","3009","5007"),
-  ("70007","948.5","2012-09-10","3005","5005"),
-  ("70005","2400.6","2012-07-27","3007","5006");
+INSERT INTO DEPARTMENT (EMPLOYEE_ID, NAME, DEPARTMENT_ID, MANAGER_ID, SALARY) VALUES
+('100', 'STEVEN KING', '90', '100', 24000),
+('101', 'NEENA KOCHCAR', '90', '100', 17000),
+('102', 'LEX DEHAAN', '90', '102', 9000),
+('103', 'BRUCE LEE', '60', '103', 4800),
+('104', 'DIANA WILLS', '60', '103', 25000),
+('105', 'VALLI PATOR', '50', '100', 4200),
+('1973', 'LUV HAMI', '60', '102', 5000),
+('106', 'DAVID AUSTIN', '90', '100', 6000);
