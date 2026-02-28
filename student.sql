2648ef4f1c08621b5bb873de6de0bc87a5dfb02d
ArindamBhatt <57433071+arindam9864@users.noreply.github.com>
2026-02-28 21:14:13 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -1,9 +1,8 @@
-body {
-    margin: 0;
-    font-family: Arial;
-}
-
-.topnav {
-    overflow: hidden;
-    background-color: #333;
-}
+INSERT INTO Salesman(Salesman_id,name,city,Comission)
+VALUES
+  ("5001","James Hoog","New York","0.15"),
+  ("5002","Nail Knite","Paris","0.13"),
+  ("5005","Pit Alex","London","0.11"),
+  ("5006","Mc Lyon","Paris","0.14"),
+  ("5007","Paul Adam","Rome","0.13"),
+  ("5003","Lauson Hen","San Jose","0.12");
