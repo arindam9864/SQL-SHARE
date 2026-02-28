f29f0d9a41578837b26779d71664e6528382f141
ArindamBhatt <57433071+arindam9864@users.noreply.github.com>
2026-02-28 14:23:16 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -1,16 +1,10 @@
-CREATE TABLE IF NOT EXISTS Salesman(
-  Salesman_id TEXT PRIMARY KEY,
-  name TEXT,
-  city TEXT,
-  commission REAL
-);
-
-
-INSERT INTO Salesman(Salesman_id,name,city,commission)
+INSERT INTO Customer(customer_id,cust_name,city,grade,Salesman_id)
 VALUES
-  ("5001","James Hoog","New York",0.15),
-  ("5002","Nail Knite","Paris",0.13),
-  ("5005","Pit Alex","London",0.11),
-  ("5006","Mc Lyon","Paris",0.14),
-  ("5007","Paul Adam","Rome",0.13),
-  ("5003","Lauson Hen","San Jose",0.12);
+  ("3002","Nick Rimando","New York",100,"5001"),
+  ("3007","Brad Davis","New York",200,"5001"),
+  ("3005","Graham Zusi","California",200,"5002"),
+  ("3008","Julian Green","London",300,"5002"),
+  ("3004","Fabian Johnson","Paris",300,"5006"),
+  ("3009","Geoff Cameron","Berlin",100,"5003"),
+  ("3003","Jozy Altidore","Moscow",200,"5007"),
+  ("3001","Brad Guzan","London",NULL,"5005");
