21145365d55b6bab44f51b6bedc1f5042c15db6b
ArindamBhatt <57433071+arindam9864@users.noreply.github.com>
2026-02-28 21:25:04 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -1,10 +1,8 @@
-INSERT INTO Customer(customer_id,cust_name,city,grade,Salesman_id)
+INSERT INTO Orders(ord_no,purch_amt,ord_date,customer_id,Salesman_id)
 VALUES
-  ("3002","nick rimando","new york","100","5001"),
-  ("3007","brad davis","new york","200","5001"),
-  ("3005","graham zusi","california","200","5002"),
-  ("3008","julian green","london","300","5002"),
-  ("3004","fabian johnson","paris","300","5006"),
-  ("3009","geoff cameron","berlin","100","5003"),
-  ("3003","jozy altidor","moscow","200","5007"),
-  ("3001","brad guzan","london","","5005");
+  ("70001","150.5","2012-10-05","3005","5002"),
+  ("70009","270.65","2012-09-10","3001","5001"),
+  ("70002","65.26","2012-10-05","3002","5003"),
+  ("70004","110.5","2012-08-17","3009","5007"),
+  ("70007","948.5","2012-09-10","3005","5005"),
+  ("70005","2400.6","2012-07-27","3007","5006");
