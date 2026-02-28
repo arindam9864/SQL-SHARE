518780294fd0ebab887dbbb6db650631ddfe4a27
ArindamBhatt <57433071+arindam9864@users.noreply.github.com>
2026-02-28 14:10:18 +0530

    Update student.sql

--- student.sql
+++ student.sql
@@ -1,17 +1,8 @@
-CREATE TABLE IF NOT EXISTS DEPARTMENT (
-    EMPLOYEE_ID TEXT,
-    NAME TEXT,
-    DEPARTMENT_ID TEXT,
-    MANAGER_ID TEXT,
-    SALARY REAL
-);
-
-INSERT INTO DEPARTMENT (EMPLOYEE_ID, NAME, DEPARTMENT_ID, MANAGER_ID, SALARY) VALUES
-('100', 'STEVEN KING', '90', '100', 24000),
-('101', 'NEENA KOCHCAR', '90', '100', 17000),
-('102', 'LEX DEHAAN', '90', '102', 9000),
-('103', 'BRUCE LEE', '60', '103', 4800),
-('104', 'DIANA WILLS', '60', '103', 25000),
-('105', 'VALLI PATOR', '50', '100', 4200),
-('1973', 'LUV HAMI', '60', '102', 5000),
-('106', 'DAVID AUSTIN', '90', '100', 6000);
+INSERT INTO Salesman(Salesman_id,name,city,commission)
+VALUES
+  ("5001","James Hoog","New York",0.15),
+  ("5002","Nail Knite","Paris",0.13),
+  ("5005","Pit Alex","London",0.11),
+  ("5006","Mc Lyon","Paris",0.14),
+  ("5007","Paul Adam","Rome",0.13),
+  ("5003","Lauson Hen","San Jose",0.12);
